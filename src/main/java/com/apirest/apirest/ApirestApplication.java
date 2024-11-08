2e9bb9ee3d400dd0abcc82e8e65fb17fcfbb819e
devencarnacion <jcencarancion17@outlook.es>
2024-11-07 22:07:05 -0400

    Realizamos un CRUD servidor a través de una API REST realizada en JAVA utilizando Spring Boot, Hibernate y PostgreSQL

---
+++ src/main/java/com/apirest/apirest/ApirestApplication.java
@@ -0,0 +1,16 @@
+package com.apirest.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestApplication.class, args);
+	}
+
+}
+// Repositorios son componenetes que interactuan con la capa de persisitencia de datos
+
+// Entidades representan objetos que vamos a utilizar 
